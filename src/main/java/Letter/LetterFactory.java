03b162d3ba7b15b93712f1987dbb5397d3df388b
Ilyas Bensaha <ilyas.bensaha@polymtl.ca>
2024-01-31 13:46:41 -0500

    Le message "BONNE CHANCE" s'affiche dans l'image "all.jpg". Tous les tests passent!

--- src/main/java/Letter/LetterFactory.java
+++ src/main/java/Letter/LetterFactory.java
@@ -17,7 +17,21 @@ public final class LetterFactory {
      * @return BaseShape containing the letter A
      */
     public static BaseShape create_A()  {
-        return null;
+        BaseShape shapeA = new BaseShape();
+        Rectangle rectangle1 = new Rectangle(1.0, 120.0);
+        Rectangle rectangle2 = rectangle1.clone();
+        Rectangle rectangle3 = new Rectangle(30.0, 1.0);
+
+        rectangle1.rotate(rectangle1.getCoords(), Math.PI/16);
+        rectangle2.rotate(rectangle2.getCoords(), -Math.PI/16);
+        rectangle1.translate(rectangle1.getCoords(), new Point2d(-12.0, 0.0));
+        rectangle2.translate(rectangle2.getCoords(), new Point2d(12.0, 0.0));
+        rectangle3.translate(rectangle3.getCoords(), new Point2d(2.0, 20.0));
+
+        shapeA.add(rectangle1);
+        shapeA.add(rectangle2);
+        shapeA.add(rectangle3);
+        return shapeA;
     }
 
     /** TODO
@@ -25,7 +39,18 @@ public final class LetterFactory {
      * @return BaseShape containing the letter B
      */
     public static BaseShape create_B() {
-        return null;
+        BaseShape shapeB = new BaseShape();
+        Ellipse ellipse1 = new Ellipse(40.0, 30.0);
+        Ellipse ellipse2 = ellipse1.clone();
+        Rectangle rectangle1 = new Rectangle(20.0, 60.0);
+
+        ellipse2.translate(ellipse2.getCoords(), new Point2d(0.0, 30.0));
+        rectangle1.translate(rectangle1.getCoords(), new Point2d(-15.0, 15.0));
+
+        shapeB.add(ellipse1);
+        shapeB.add(ellipse2);
+        shapeB.add(rectangle1);
+        return shapeB;
     }
 
     /** TODO
@@ -33,7 +58,15 @@ public final class LetterFactory {
      * @return BaseShape containing the letter C
      */
     public static BaseShape create_C() {
-        return null;
+        BaseShape shapeC = new BaseShape();
+        Ellipse ellipse1 = new Ellipse(40.0, 60.0);
+        Rectangle square1 = new Square(40.0);
+
+        square1.translate(square1.getCoords(), new Point2d(10.0, 0.0));
+        ellipse1.remove(square1);
+
+        shapeC.add(ellipse1);
+        return shapeC;
     }
 
     /** TODO
@@ -41,7 +74,21 @@ public final class LetterFactory {
      * @return BaseShape containing the letter E
      */
     public static BaseShape create_E() {
-        return null;
+        BaseShape shapeE = new BaseShape();
+        Rectangle rectangle1 = new Rectangle(4.0, 60.0);
+        Rectangle rectangle2 = new Rectangle(40.0, 4.0);
+        Rectangle rectangle3 = new Rectangle(30.0, 4.0);
+        Rectangle rectangle4 = rectangle2.clone();
+
+        rectangle1.translate(rectangle1.getCoords(), new Point2d(-20.0, 0.0));
+        rectangle2.translate(rectangle2.getCoords(), new Point2d(0.0, -30.0));
+        rectangle4.translate(rectangle4.getCoords(), new Point2d(0.0, 30.0));
+
+        shapeE.add(rectangle1);
+        shapeE.add(rectangle2);
+        shapeE.add(rectangle3);
+        shapeE.add(rectangle4);
+        return shapeE;
     }
 
     /** TODO
@@ -49,7 +96,18 @@ public final class LetterFactory {
      * @return BaseShape containing the letter H
      */
     public static BaseShape create_H() {
-        return null;
+        BaseShape shapeH = new BaseShape();
+        Rectangle rectangle1 = new Rectangle(4.0, 60.0);
+        Rectangle rectangle2 = rectangle1.clone();
+        Rectangle rectangle3 = new Rectangle(40.0, 4.0);
+
+        rectangle1.translate(rectangle1.getCoords(), new Point2d(-20.0, 0.0));
+        rectangle2.translate(rectangle2.getCoords(), new Point2d(20.0, 0.0));
+
+        shapeH.add(rectangle1);
+        shapeH.add(rectangle2);
+        shapeH.add(rectangle3);
+        return shapeH;
     }
 
     /** TODO
@@ -57,7 +115,19 @@ public final class LetterFactory {
      * @return BaseShape containing the letter N
      */
     public static BaseShape create_N() {
-        return null;
+        BaseShape shapeN = new BaseShape();
+        Rectangle rectangle1 = new Rectangle(4.0, 60.0);
+        Rectangle rectangle2 = rectangle1.clone();
+        Rectangle rectangle3 = rectangle1.clone();
+
+        rectangle1.translate(rectangle1.getCoords(), new Point2d(-20.0, 0.0));
+        rectangle2.translate(rectangle2.getCoords(), new Point2d(20.0, 0.0));
+        rectangle3.rotate(rectangle3.getCoords(), Math.PI - Math.PI/6);
+
+        shapeN.add(rectangle1);
+        shapeN.add(rectangle2);
+        shapeN.add(rectangle3);
+        return shapeN;
     }
 
     /** TODO
@@ -65,7 +135,7 @@ public final class LetterFactory {
      * @return BaseShape containing the letter O
      */
     public static BaseShape create_O() {
-        return null;
+        return new Ellipse(40.0, 60.0);
     }
 
 }
